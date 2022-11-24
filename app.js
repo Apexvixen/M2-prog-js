c079027d1bd0e64fe8f43263a3083b543326a5f7
ythiero <35019@ma-web.nl>
2022-11-24 12:28:51 +0100

    code

---
+++ app.js
@@ -0,0 +1,25 @@
+class app
+{
+    runapplication()
+    {
+        console.log("hello world");
+        let appnaam = "app.js";
+        let versienummer = "0.9";
+        let versiedatum  = "24/11/2022";
+        let autheur  = "ytshiero bruce";
+        let copyright = "dunno fam";
+        let distributeur = "no monky";
+        let darkmode = "bool";
+        console.log("hello world");
+        console.log("0.9");
+        console.log("24/11/2022");
+        console.log("ytshiero bruce");
+        console.log("dunno fam");
+        console.log("no monky");
+        console.log("bool");
+        
+    }
+    
+}
+let app = new app();
+app.runapplication();
